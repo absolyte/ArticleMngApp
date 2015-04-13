4ef4ce70c083234e44700e8b620aeab14ad6b633
absolyte@mail.ru <absolyte@mail.ru>
2015-04-14 00:29:35 +0300

    MVC started

---
+++ ArticleApp/Models/ArticleModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArticleApp.Models
+{
+    public class ArticleModel
+    {
+        public int ArticleId { get; set; }
+
+        [Required]
+        [Display(Name = "Article title")]
+        public string articleTitle { get; set;}
+
+        [Required]
+        [Display(Name = "Article content")]
+        public string articleContent { get; set;}
+
+
+        public int author { get; set; }
+
+        public int articleAuthor { get; set; }
+    }
+}
---
+++ TestRunner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.ConsoleRunner;
+using NUnit.Core;
+
+namespace TestRunner
+{
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            InternalTrace.Initialize("nunit-console_%p, log",(InternalTraceLevel)1);
+            return Runner.Main(args);
+        }
+    }
+}
