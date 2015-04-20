d17608afb0db23c88aac6624e0f46f65ef2d42f2
absolyte@mail.ru <absolyte@mail.ru>
2015-04-21 01:35:08 +0300

    Trying rsvp form

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "good morning" : "good afternooon";
+            return View();
+        }
+
+       [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/GuestResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    using System.Net.Configuration;
+    using System.Runtime.Remoting.Services;
+
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "please, input your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please input email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "please Enter valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please, input phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please choose one option yes or no")]
+        public bool? WillAttend { get; set; }
+    }
+}
